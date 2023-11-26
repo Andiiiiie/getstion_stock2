3dad273391995177c3ebd27ef70e685f73f9a4d7
Andiiiiie <andiejessica2020@gmail.com>
2023-11-26 20:56:43 +0300

    Initial commit

---
+++ src/main/java/com/example/getstion_stock2/GetstionStock2Application.java
@@ -0,0 +1,13 @@
+package com.example.getstion_stock2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetstionStock2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetstionStock2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getstion_stock2/GetstionStock2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getstion_stock2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetstionStock2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
